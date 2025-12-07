86f3de2c44c8b2a8c6465aebe1c347c90e5bd0c0
Jamie Barker <dodgerbarker@gmail.com>
2025-12-07 23:27:50 +0000

    day 2

---
+++ aoc/day_02.py
@@ -0,0 +1,61 @@
+def part_one(file_path: str):
+    """[summary]
+
+    Args:
+        file_path (str): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    # read file
+    with open(file_path) as f:
+        lines = f.readlines()
+
+    total = 0
+    for a,b in [x.strip().split("-") for x in lines[0].split(",")]:
+        for i in range(int(a), int(b)+1):
+            i_str = str(i)
+            if i_str[:len(i_str)//2] == i_str[len(i_str)//2:]:
+                total += i
+    return total
+
+def _is_invalid(i_str, l):
+    if len(i_str) % l != 0:
+        return False
+
+    init_i_str = i_str[:l]
+    for k in range(l, len(i_str), l):
+        if i_str[k:k+l] != init_i_str:
+            return False
+    return True
+
+
+def part_two(file_path: str):
+    """[summary]
+
+    Args:
+        file_path (str): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    with open(file_path) as f:
+        lines = f.readlines()
+    
+    total = 0
+    for a,b in [x.strip().split("-") for x in lines[0].split(",")]:
+        for i in range(int(a), int(b)+1):
+            i_str = str(i)
+            for j in range(1, len(i_str)//2+1):
+                if _is_invalid(i_str, j):
+                    total += i
+                    break
+
+    return total
+
+
+if __name__ == "__main__":
+    print(part_one("aoc/inputs/day_02.txt"))
+    print(part_two("aoc/inputs/day_02.txt"))
