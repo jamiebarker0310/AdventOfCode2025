d536fe85438565d6a8006e783eb9737f8eb9f18c
Jamie Barker <dodgerbarker@gmail.com>
2025-12-09 22:53:32 +0000

    linting

--- aoc/day_06.py
+++ aoc/day_06.py
@@ -1,5 +1,6 @@
 import math
 
+
 def part_one(file_path: str):
     """[summary]
 
@@ -13,11 +14,11 @@ def part_one(file_path: str):
     # read file
     with open(file_path) as f:
         lines = f.readlines()
-    
-    grid = [[x for x in line.split(" ") if x.strip()!=""] for line in lines]
+
+    grid = [[x for x in line.split(" ") if x.strip() != ""] for line in lines]
     total = 0
     for j in range(len(grid[0])):
-        digits = [int(grid[i][j]) for i in range(len(grid)-1)]
+        digits = [int(grid[i][j]) for i in range(len(grid) - 1)]
         if grid[-1][j] == "+":
             total += sum(digits)
         else:
@@ -43,12 +44,11 @@ def part_two(file_path: str):
     digits = []
     total = 0
     for j in range(len(lines[0])):
-        column = "".join([lines[i][-j-1].strip() for i in range(len(lines)-1)])
+        column = "".join([lines[i][-j - 1].strip() for i in range(len(lines) - 1)])
 
         if column:
-            digit = int(column)
             digits.append(int("".join(column)))
-        operation = lines[-1][-j-1]
+        operation = lines[-1][-j - 1]
 
         if operation.strip():
             if operation == "+":
