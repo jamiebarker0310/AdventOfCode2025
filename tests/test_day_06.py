a70442f6385d2f296e80e7cf88cdaddec2543989
Jamie Barker <dodgerbarker@gmail.com>
2025-12-09 22:51:46 +0000

    day 6

---
+++ tests/test_day_06.py
@@ -0,0 +1,13 @@
+from aoc.day_06 import part_one, part_two
+
+def test_part_one():
+
+    test_file_path = "tests/test_inputs/test_day_06.txt"
+
+    assert part_one(test_file_path) == 4277556
+
+def test_part_two():
+
+    test_file_path = "tests/test_inputs/test_day_06.txt"
+
+    assert part_two(test_file_path) == 3263827
